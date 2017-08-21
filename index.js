519c37952a56d8be9a0230e46c83a24dc40e726a
Tristan Schmitt <trschmitt50@live.com>
2017-08-21 11:45:40 -0400

    Added Success page

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const expressValidator = require("express-validator");
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const login = require("./loginRoutes");
 
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
@@ -19,29 +20,19 @@ app.use(morgan("dev"));
 
 app.use(cookieParser());
 
-app.use(require("./loginRoutes"));
-
-app.use(session({
-  secret: 's1744439857-jshaf348957uo-eusjdflhaksjhd',
-  resave: false,
-  saveUninitialized: false
-}));
-
+app.use((req, res, next) => {
+  if (!req.session.views) {
+    req.session.views = 0;
+  }
+  req.session.views += 1;
+  next();
+})
 
 let user = {
-  name: "user",
+  name: "person",
   email: "email@email.com",
   password: "password"
 };
-
-app.use("/", (req, res, next) => {
-	console.log(req.sessions);
-	if (!req.session.views) {
-		req.session.views = 0;
-	}
-	req.session.views += 1;
-	next();
-})
 
 app.use("/", (req, res, next) => {
   if (req.body.name === user.name) {
@@ -52,6 +43,23 @@ app.use("/", (req, res, next) => {
   next();
 })
 
+app.use(session({
+  secret: 's1744439857-jshaf348957uo-eusjdflhaksjhd',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use((req, res, next) => {
+  if (req.body.password === user.email) {
+    if (req.body.email === user.email) {
+      req.session.login = true;
+      req.session = user.name;
+    }
+  }
+  next();
+})
+
+app.use(login);
 
 app.listen(3010, () => {
 	console.log("Successfully running at http://localhost:3010");
--- loginRoutes.js
+++ loginRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
-module.exports = router;
+
 
 router.get("/", (req, res) => {
-  res.render("index", {});
+  if (req.session.login === true) {
+    res.redirect("/success");
+  } else {
+    res.render("index", {});
+  }
 })
 
+router.get("/success", (req, res) => )
 
 router.post("/form", (req, res) => {
-	console.log("New Request", req.body);
-
-//Can't figure out how to get the session and cookie to transfer to the router branch and then show that user name and store the cookie to be used as an identifier.
-  console.log(req.session.name);
   req.checkBody("name", "You must provide a valid User name.").notEmpty();
 	req.getValidationResult().then((result) => {
 		if (!result.isEmpty()) {
@@ -21,3 +22,5 @@ router.post("/form", (req, res) => {
 		res.redirect("/");
 	});
 });
+
+module.exports = router;
