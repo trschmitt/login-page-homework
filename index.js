906bfd4576d63dbe978286708463863e63e0e03b
Tristan Schmitt <trschmitt50@live.com>
2017-08-21 12:39:09 -0400

    session is being stored successfully

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const session = require('express-session');
-const mustacheExpress = require('mustache-express');
+const session = require("express-session");
+const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
 const expressValidator = require("express-validator");
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const login = require("./loginRoutes");
+
+const user = {
+  name: "person",
+  email: "email@email.com",
+  password: "password"
+};
 
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
 app.set("views", __dirname + "/views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(expressValidator());
 
@@ -20,47 +27,35 @@ app.use(morgan("dev"));
 
 app.use(cookieParser());
 
+
+app.use(
+  session({
+    secret: "s1744439857-jshaf348957uo-eusjdflhaksjhd",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
 app.use((req, res, next) => {
   if (!req.session.views) {
     req.session.views = 0;
   }
   req.session.views += 1;
   next();
-})
-
-let user = {
-  name: "person",
-  email: "email@email.com",
-  password: "password"
-};
-
-app.use("/", (req, res, next) => {
-  if (req.body.name === user.name) {
-    req.session.name = req.body.name;
-    console.log(req.session);
-    return;
-  }
-  next();
-})
-
-app.use(session({
-  secret: 's1744439857-jshaf348957uo-eusjdflhaksjhd',
-  resave: false,
-  saveUninitialized: false
-}));
+});
 
 app.use((req, res, next) => {
-  if (req.body.password === user.email) {
+  if (req.body.password === user.password) {
     if (req.body.email === user.email) {
       req.session.login = true;
-      req.session = user.name;
+      req.session.name = user.name;
     }
   }
   next();
-})
+});
 
 app.use(login);
 
 app.listen(3010, () => {
-	console.log("Successfully running at http://localhost:3010");
+  console.log("Successfully running at http://localhost:3010");
 });
--- loginRoutes.js
+++ loginRoutes.js
@@ -1,6 +1,5 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
 
 router.get("/", (req, res) => {
   if (req.session.login === true) {
@@ -8,19 +7,31 @@ router.get("/", (req, res) => {
   } else {
     res.render("index", {});
   }
-})
+});
 
-router.get("/success", (req, res) => )
+router.get("/loggedIn", (req, res) => {
+  console.log("Working?");
+  if (req.session.login === true) {
+    res.render("success", { user: req.session });
+  } else {
+    res.redirect("/");
+  }
+});
 
 router.post("/form", (req, res) => {
+  if (req.session.login === true) {
+    res.redirect("/loggedIn");
+  } else {
+    res.redirect("/");
+  }
   req.checkBody("name", "You must provide a valid User name.").notEmpty();
-	req.getValidationResult().then((result) => {
-		if (!result.isEmpty()) {
-			res.render("index", {error: "MISTAKE!! Please enter a name."});
-			return;
-		}
-		res.redirect("/");
-	});
+  req.getValidationResult().then(result => {
+    if (!result.isEmpty()) {
+      res.render("index", { error: "MISTAKE!! Please enter a name." });
+      return;
+    }
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
